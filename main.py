018ec73cce5752e6a6e132462621ea82b3e94d07
Luan Torres <luantorres360@gmail.com>
2020-08-03 17:46:35 -0300

    Tela de Erro Responsiva + URL Válida

--- main.py
+++ main.py
@@ -5,16 +5,23 @@ app = Flask(__name__)
 @app.route("/", methods=["POST","GET"])
 def home():
     if request.method == "POST":
-        return redirect(url_for("results", youtubeUrl=request.form["url"]))
+        return redirect(url_for("results", youtubeUrl = request.form["url"].split('.com/')[-1]))
     else:
         return render_template("home.html")
 
 @app.route("/<youtubeUrl>/", methods=["POST","GET"])
 def results(youtubeUrl):
     if request.method == "POST":
-        return redirect(url_for("results", youtubeUrl=request.form["url"]))
-    elif 'youtube.com' in youtubeUrl:
-        return render_template("result.html", urlToHtml=youtubeUrl)
+        return redirect(url_for("results", youtubeUrl = request.form["url"]))
+    elif 'watch?v=' in youtubeUrl:
+        return render_template("result.html", urlToHtml = youtubeUrl)
+    else:
+        return redirect(url_for("wrongUrl", urlToHtml = youtubeUrl))
+
+@app.route("/teste/<urlToHtml>", methods=["POST", "GET"])
+def wrongUrl(urlToHtml):
+    if request.method == "POST":
+        return redirect(url_for("results", youtubeUrl = request.form["url"].split('.com/')[-1]))
     else:
         return render_template("erro.html")
 
